9173a871783bcfd32f3d5ea63f2f4d239d5c8583
PhuBuii <buiquangphu.work@gmail.com>
2026-02-03 14:41:57 +0700

    Update cors

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 // Enable CORS
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: [process.env.CLIENT_URL || 'http://localhost:5173', 'http://yourblog.awsome-project.store'],
     credentials: true
 }));
 
